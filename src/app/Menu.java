ce811b7e5542ead5eb46f12b777b8a146fa2a25d
Atif Siddiqui <itsatifsiddiqui@gmail.com>
2019-05-31 08:47:23 +0500

    update

---
+++ src/app/Menu.java
@@ -0,0 +1,46 @@
+package app;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Menu extends JFrame {
+
+    private JButton addButton, searchButton;
+
+    public Menu() {
+
+        setTitle("Main Menu");
+        setSize(640, 480);
+        setLayout(new GridLayout(2, 1));
+        setVisible(true);
+
+        addButton = new JButton("Add Student");
+        searchButton = new JButton("Search Student");
+
+        add(addButton);
+        add(searchButton);
+
+        addButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("ADD");
+
+            }
+        });
+
+        searchButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Search");
+
+            }
+        });
+    }
+
+}
---
+++ src/app/StudentForm.java
@@ -0,0 +1,14 @@
+package app;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class StudentForm extends JFrame {
+
+
+    JLabel username;
+    JLabel username;
+    
+
+    
+}
